ea60c182d9efad899c289d0bfc82ac9ba1120b22
henryf <fenghl@anmed.com.cn>
2025-05-27 05:42:38 +1200

    add new entity interface for batch processing

---
+++ src/biz/BatchRecord.ts
@@ -0,0 +1,18 @@
+export default interface BatchRecord<T> {
+
+    /**
+     * 记录号，通常是行号
+     */
+    recNo: number;
+
+    /**
+     * 数据
+     */
+    data: T;
+
+    /**
+     * 处理异常
+     */
+    error: any;
+
+}
